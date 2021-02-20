727a11dc1cb7416c31a7e60d127e6e814560dae0
Jimmy <slimjim49j@gmail.com>
2021-02-20 16:04:12 -0700

    Change moyai give command to only use moyai emojis

--- main.py
+++ main.py
@@ -34,12 +34,26 @@ async def ping(ctx):  # when command "ping" is typed
     await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
 
 
+
+
+emojis = []
+
+# not really sure what that for loop does, more info
+# https://towardsdatascience.com/filtering-lists-in-python-a3387c7b6b5e
+async def get_moyai_emojis():
+    guild = await client.fetch_guild(801989557309538325)
+    guild_emojis = guild.emojis
+    return [emoji for emoji in guild_emojis if "moy" in emoji.name]
+
+# using global vars:
+# https://www.w3schools.com/python/python_variables_global.asp
 @client.command(aliases=['please', 'want'])
 async def give(ctx, *, statement):  # * allows you to take multiple arguments
-    responses = ['Moyai',
-                 'Epic moyai',
-                 ':moyai:']
-    await ctx.send(f'You just said: {statement}\n And for that I will give you: {random.choice(responses)}')
+    global emojis
+    if not emojis:
+        emojis = await get_moyai_emojis()
+
+    await ctx.send(f'You just said: {statement}\n And for that I will give you: {random.choice(emojis)}')
 
 client.run(os.getenv("TOKEN"))
 
